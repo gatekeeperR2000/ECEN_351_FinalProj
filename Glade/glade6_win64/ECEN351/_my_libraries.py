7cafa78adb8bd5be2e331acfd32edb9e3eee169f
Rylan Jurgens <122000590+gatekeeperR2000@users.noreply.github.com>
2025-04-01 10:18:01 -0600

    adding Final Project Folder in ECEN351, making blank cell folders

--- Glade/glade6_win64/ECEN351/_my_libraries.py
+++ Glade/glade6_win64/ECEN351/_my_libraries.py
@@ -3,7 +3,7 @@ from ui import *
 gui=cvar.uiptr
 
 # Opening Libs
-mylibs = ["C5N", "PCELL", "C5N_std_lib", "C5N_std_lib_bad", "C5N_std_lib_chk", "buffer_example"]
+mylibs = ["C5N", "PCELL", "C5N_std_lib", "C5N_std_lib_bad", "C5N_std_lib_chk", "buffer_example", "ECEN_351_FinalProj"]
 nlib = len(mylibs)
 libinit = [0 for i in range(nlib)]
 for n in range(nlib):
